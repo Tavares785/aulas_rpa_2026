546a3495b64befd5da64a708202f9719376ddb50
SamukkaX <samukkajr@gmail.com>
2026-08-18 22:17:31 -0300

    TF Aula 01

---
+++ AULA01/bot_initializer.py
@@ -0,0 +1,16 @@
+BOT_NAME: str = "Robo_01"
+MAX_RETRIES: int = 3
+EXECUTION_TIMEOUT: float = 10
+IS_PRODUCTION: bool = True
+
+print(BOT_NAME)
+print(type(BOT_NAME))
+
+print(MAX_RETRIES)
+print(type(MAX_RETRIES))
+
+print(EXECUTION_TIMEOUT)
+print(type(EXECUTION_TIMEOUT))
+
+print(IS_PRODUCTION)
+print(type(IS_PRODUCTION))
