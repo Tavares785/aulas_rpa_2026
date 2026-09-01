105083a6465556f8544b3a066340ddb6a8ea62b6
UmaExistencia <rma56743@gmsil.com>
2026-09-01 20:16:49 -0300

    TF Aula 01

---
+++ AULA01/bot_initializer.py
@@ -0,0 +1,9 @@
+bot_name= "Duck 2.0"
+max_retries = 3
+execution_timeout = 35.555555555
+is_production = True
+
+print(f"bot_name: {bot_name} | Type: {type(bot_name)}")
+print(f"max_retries: {max_retries} | Type: {type(max_retries)}")
+print(f"execution_timeout: {execution_timeout} | Type: {type(execution_timeout)}")
+print(f"is_production: {is_production} | Type: {type(is_production)}")
