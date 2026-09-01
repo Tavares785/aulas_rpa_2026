84c40123f83d03a7dc205ced42a6dce066a0595b
Gabriel Rubião <gabrielhrubiao@gmail.com>
2026-09-01 19:49:16 -0300

    Remove extra newline in validador_transacoes.py

--- AULA02/validador_transacoes.py
+++ AULA02/validador_transacoes.py
@@ -1,6 +1,5 @@
 # Lista de transações fornecida no enunciado
 transacoes = [150.0, 3200.5, 12500.0, 450.0, -50.0, 800.0, 0]
-
 # Percorre a lista de transações
 for valor in transacoes:
     # 1. Verifica transações inválidas ou negativas (Erro Crítico)
