95e73ee84c928d0936638fdef0e9410f1a5fab06
Vinicius <vinigamaro1@gmail.com>
2026-09-09 01:54:43 -0300

    vinicius amaro 6326015

---
+++ AULA03/main.py
@@ -0,0 +1,33 @@
+from mod_rh import cadastrar_colaborador, exibir_colaboradores
+
+lista_colaboradores = []
+
+while True:
+    print("\n--- MENU ---")
+    print("1 - Cadastrar")
+    print("2 - Listar")
+    print("0 - Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        nome = input("Digite o nome: ")
+        cargo = input("Digite o cargo: ")
+        salario = float(input("Digite o salário: "))
+
+        colaborador = cadastrar_colaborador(nome, cargo, salario)
+        lista_colaboradores.append(colaborador)
+
+        print("Colaborador cadastrado com sucesso!")
+
+    elif opcao == "2":
+        exibir_colaboradores(lista_colaboradores)
+
+    elif opcao == "0":
+        print("Programa encerrado.")
+        break
+
+    else:
+        print("Opção inválida.")
+
+        
