f27f46dd04f806f5559d1bdd0c1b50ca9e7d4df3
Manu Costa <emanuelacosta.contato@gmail.com>
2026-09-15 01:50:46 -0300

    Criando script bot_initializer do Lab 01

---
+++ AULA01/bot_initializer.py
@@ -0,0 +1,8 @@
+nome = "bot_test1"
+quantidade = 7000
+taxa_de_sucesso = 9.0
+
+print("---- STATUS DO ROBÔ ----")
+print(f"Nome do meu primeiro bot {nome}")
+print(f"Quantidade processada sera de {quantidade}")
+print(f"E a quantidade de suceeso será de {taxa_de_sucesso}%")
